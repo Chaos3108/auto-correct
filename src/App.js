c5ef09934c13792d9679e7452ee958cb85730f9b
yuva kishore <yuvakishorekolli@gmail.com>
2025-06-16 14:58:13 +0530

    Added code for auto correct

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import React, { useState } from "react";
 
 function App() {
+  const [text,setText] = useState("");
+  const customDictionary = {
+    teh: "the",
+    wrok: "work",
+    fot: "for",
+    exampl: "example",
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>Spell check and Auto-correction</h1>
+      <textarea
+        rows="10"
+        cols="50"
+        placeholder="Enter text"
+        spellCheck="true"
+        autoCorrect="on"
+        autoCapitalize="sentences"
+        value={text}
+        setText={setText}
+        onChange={(e) => setText(e.target.value)}
+        className="text-input"
+      ></textarea>
+      {text && (
+        <div className="suggestions">
+          <ul>
+            {text.split(" ").map((word, index) => {
+              const correctedWord = customDictionary[word.toLowerCase()];
+              return (
+                <>
+                  {correctedWord ? (
+                    <span>
+                      {"Did you mean:"}  {correctedWord}?
+                    </span>
+                  ) :(null)}
+                  </>
+              );
+            })}
+          </ul>
+        </div>
+      )}
     </div>
   );
 }
